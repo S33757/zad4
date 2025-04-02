530ec6a4fbe306907cbc151fa79771356359f90e
S33757 <s33757@pjwstk.edu.pl>
2025-04-02 22:02:12 +0200

    zrobilem klase Klient i dodalem 1

--- Klient.java
+++ Klient.java
@@ -1,5 +1,29 @@
 public class Klient {
-    public static void main(String[] args) {
-        System.out.println("Tu beda klienci");
+    private int idKlient;
+    private String imie;
+    private String nazwisko;
+    private String email;
+    private boolean czyStaly;
+
+    public Klient(int idKlient, String imie, String nazwisko, String email, boolean czyStaly) {
+        this.idKlient = idKlient;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.email = email;
+        this.czyStaly = czyStaly;
+    }
+
+    public int getIdKlient() {return idKlient;}
+    public String getImie() {return imie;}
+    public String getNazwisko() {return nazwisko;}
+    public String getEmail() {return email;}
+    public boolean isCzyStaly() {return czyStaly;}
+
+    public void wyswietlKlient() {
+        System.out.println("ID Klienta: " + idKlient);
+        System.out.println("Imie: " + imie);
+        System.out.println("Nazwisko: " + nazwisko);
+        System.out.println("Email: " + email);
+        System.out.println("CzyStaly: " + czyStaly);
     }
 }
--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@ public class Main {
         SklepKomputerowy sklep = new SklepKomputerowy();
 
         Produkt laptop = new Produkt(1, "Laptop Lenovo", "Laptop", 5000, 10);
+        Klient Hubert = new Klient(1, "Hubert", "Bogucki", "s33757@pjwstk.edu.pl", true);
 
         laptop.wyswietlInformacje();
+        Hubert.wyswietlKlient();
     }
 }
