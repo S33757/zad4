8d6e258b75f4d29821680fe02f137dd1aef2c61b
S33757 <s33757@pjwstk.edu.pl>
2025-04-03 01:31:30 +0200

    dodalem przyklady uzycia

--- Main.java
+++ Main.java
@@ -6,14 +6,45 @@ public class Main {
         Produkt laptop = new Produkt(1, "Laptop Lenovo", "Laptop", 5000, 10);
         sklep.dodajProdukt(laptop);
 
+        Produkt mysz = new Produkt(2, "Mysz Logitech", "Mysz", 500, 120);
+        sklep.dodajProdukt(mysz);
+
+        Produkt monitor = new Produkt(3, "Monitor Iiyama", "Monitor", 1500, 75);
+        sklep.dodajProdukt(monitor);
+
         //Klienci------------------------------------------------------------------------------------------------------------------
-        Klient Hubert = new Klient(1, "Hubert", "Bogucki", "s33757@pjwstk.edu.pl", false);
+        Klient Hubert = new Klient(1, "Hubert", "Bogucki", "s33757@pjwstk.edu.pl", true);
         sklep.dodajKlienta(Hubert);
+
+        Klient Adam = new Klient(2, "Adam", "Nowak", "adam.nowak@gmail.com", false);
+        sklep.dodajKlienta(Adam);
+
+        Klient Klaudia = new Klient(3, "Klaudia", "Kowalska", "klaudia.kowalska@gmail.com", true);
+        sklep.dodajKlienta(Klaudia);
+
+        Klient Rafal = new Klient(4, "Rafal", "Szybki", "rafal.szybki@gmail.com", false);
+        sklep.dodajKlienta(Rafal);
 
         //Zamowienia-------------------------------------------------------------------------------------------------
         Zamowienie pierwsze = sklep.utworzZamowienie(Hubert, new Produkt[]{laptop}, new int[]{1});
+        System.out.println("PIERWSZE ZAMOWIENIE: ");
         pierwsze.wyswietlSzczegoly();
         pierwsze.obliczWartoscZamowienia();
+
+        Zamowienie drugie = sklep.utworzZamowienie(Adam, new Produkt[]{mysz, monitor}, new int[]{1, 2});
+        System.out.println("DRUGIE ZAMOWIENIE: ");
+        drugie.wyswietlSzczegoly();
+        drugie.obliczWartoscZamowienia();
+
+        Zamowienie trzecie = sklep.utworzZamowienie(Klaudia, new Produkt[]{mysz}, new int[]{10});
+        System.out.println("TRZECIE ZAMOWIENIE: ");
+        trzecie.wyswietlSzczegoly();
+        trzecie.obliczWartoscZamowienia();
+
+        Zamowienie czwarte = sklep.utworzZamowienie(Rafal, new Produkt[]{laptop,mysz,monitor}, new int[]{3,3,3});
+        System.out.println("CZWARTE ZAMOWIENIE: ");
+        czwarte.wyswietlSzczegoly();
+        czwarte.obliczWartoscZamowienia();
 
 
         //Sprawdzenie
